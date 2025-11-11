98089c263163280bfdbe9c36f06f315078eb8b17
Poornima1126 <spoornimaa58@gmail.com>
2025-11-11 14:04:11 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,9 +5,10 @@ window._config = {
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://fwft3jhxt0.execute-api.ap-south-1.amazonaws.com/wild-ryders' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://fwft3jhxt0.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
