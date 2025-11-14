23c6a37f5fb373e6bce243e630107d8a93231560
Poornima1126 <spoornimaa58@gmail.com>
2025-11-14 11:12:47 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_pX36RCdd8', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3oc281n74kr0pj7u2tp519kund', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_LGxyXQZz0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '287nrehqnsg68gsmo4948dgk6l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://fwft3jhxt0.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
